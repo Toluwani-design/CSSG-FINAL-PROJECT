ef49a9272a0f7f2b8547c9a04d84a63c4b38a4e6
Toluwani-design <toluwaniilesanmi6@gmail.com>
2025-11-20 15:20:12 -0500

    still working

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,22 +1,10 @@
 // src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import Opportunities from "./pages/Opportunities";
-import VolunteerForm from "./pages/VolunteerForm";
+import Opportunities from "./pages/Opportunities.css";
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/opportunities" element={<Opportunities />} />
-        <Route path="/volunteer" element={<VolunteerForm />} />
-      </Routes>
-    </Router>
-  );
+  return <Opportunities />;
 }
 
 export default App;
